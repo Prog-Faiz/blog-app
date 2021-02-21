be414bb99bd595d16b7c0473388bd3703bec7633
ProgFaiz <faizx6@gmail.com>
2021-02-21 15:31:50 +0530

    created a custom hook called useFetch() and put the fetch code in Home.js to this hook"

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('could not fetch the data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsLoading(false);
-                    setError(null);
-                })
-                .catch(error => {
-                    setIsLoading(false);
-                    setError(error.message);
-                })
-        }, 2000);
-    }, []);
-
+    const { data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
+    
     return ( 
         <div className="home">
             { error && <div>{ error }</div> }
